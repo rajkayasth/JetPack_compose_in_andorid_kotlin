8c959c80798c6ff6bfa9ce6a3d62c7a56684e1d9
raj.kayasth <raj.kayasth@brainvire.com>
2022-09-15 15:16:55 +0530

    first commit

---
+++ app/src/main/java/com/example/jetpackcomposeexample/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.example.jetpackcomposeexample
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.jetpackcomposeexample.ui.theme.JetPackComposeExampleTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Row(
+                modifier = Modifier
+                    .background(Color.Green)
+                    .height(200.dp)
+                    .width(200.dp),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceEvenly
+            ) {
+                Text(text = "Raj")
+                Text(text = "Kayasth")
+                Text(text = "Kayasth")
+            }
+
+        }
+    }
+}
