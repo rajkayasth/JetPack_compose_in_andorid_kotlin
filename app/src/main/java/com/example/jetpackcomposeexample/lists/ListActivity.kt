499f34361107233fdd164cdfb68411bcca09d79d
raj.kayasth <raj.kayasth@brainvire.com>
2022-09-19 16:23:11 +0530

    - custom progressbar - list view in jetpack compose - simple animation in compose

--- app/src/main/java/com/example/jetpackcomposeexample/lists/ListActivity.kt
+++ app/src/main/java/com/example/jetpackcomposeexample/lists/ListActivity.kt
@@ -1,4 +1,58 @@
 package com.example.jetpackcomposeexample.lists
 
-class ListActivity {
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.Text
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+class ListActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+
+            LazyColumn {
+
+                itemsIndexed(
+                    listOf(
+                        "this",
+                        "is",
+                        "list",
+                        "demo",
+                        "using",
+                        "jetpack",
+                        "compose",
+                        "in",
+                        "android",
+                        "kotlin",
+                        "hope",
+                        "u will",
+                        "understand"
+                    )
+                ) { index, string ->
+                    Text(
+                        string,
+                        fontSize = 24.sp,
+                        fontWeight = FontWeight.Bold,
+                        textAlign = TextAlign.Center,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(vertical = 24.dp)
+                    )
+                }
+            }
+        }
+    }
 }
---
+++ app/src/main/java/com/example/jetpackcomposeexample/simpleanimation/SimpleAnimationActivity.kt
@@ -0,0 +1,70 @@
+package com.example.jetpackcomposeexample.simpleanimation
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.animation.animateColor
+import androidx.compose.animation.core.*
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+class SimpleAnimationActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            var sizeState by remember {
+                mutableStateOf(200.dp)
+            }
+
+            val size by animateDpAsState(
+                targetValue = sizeState,
+//                keyframes {
+//                    durationMillis = 5000
+//                    sizeState at 0 with LinearEasing
+//                    sizeState * 1.5f at 1000 with FastOutLinearInEasing
+//                    sizeState * 2f at 5000
+//                }
+                tween(
+                    durationMillis = 1000,
+                )
+//            spring(
+//                Spring.DampingRatioHighBouncy
+//            )
+            )
+
+            var infiniteTransition = rememberInfiniteTransition()
+            val color by infiniteTransition.animateColor(
+                initialValue = Color.Red,
+                targetValue = Color.Green,
+                animationSpec = infiniteRepeatable(
+                    tween(
+                        durationMillis = 2000
+                    ),
+                    repeatMode = RepeatMode.Reverse
+                    )
+            )
+            Box(
+                modifier = Modifier
+                    .size(size)
+                    .background(color),
+                contentAlignment = Alignment.Center
+            ) {
+                Button(onClick = {
+                    sizeState += 50.dp
+                }) {
+                    Text(text = "Increase Size")
+                }
+
+            }
+        }
+    }
+}
